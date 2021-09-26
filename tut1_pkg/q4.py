26b4c252ceed3cf4b68d284cbb43fcbd3b9bcd37
Rohit <L00163278@student.lyit.ie>
2021-09-26 21:28:06 +0530

    added q4

---
+++ tut1_pkg/q4.py
@@ -0,0 +1,17 @@
+# -------------------------------------------
+# Testing variables
+# (C) 2021 Rohit Mishra
+# ------------------------------------------
+
+
+# ppsn # String
+# passport number # String
+# temperature in degrees # Float
+# Miles travelled per day # Float
+# Average collection of exam grades # Float
+# Files path # String
+# Person middle initial # String
+# Biography statement # String
+# Book abstract # String
+# Mathematical function # Float
+
