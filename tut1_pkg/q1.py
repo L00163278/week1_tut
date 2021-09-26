e57cb4393907947dba7f82621807ecf0377323cc
Rohit <L00163278@student.lyit.ie>
2021-09-26 20:28:18 +0530

    added q1

---
+++ tut1_pkg/q1.py
@@ -0,0 +1,28 @@
+# -------------------------------------------
+# Testing variables.
+#
+# (C) 2021 Rohit Mishra
+
+print("\t*2+Title")
+print("Hello")
+print("Hello World")
+a = 5
+b = 12.3456
+c = a + b
+d = "A number" + "b"
+
+# error at line 12, proposed solution : "b"
+e = a + \
+    b + \
+    c
+f = 2
+g = a/f
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+
+
