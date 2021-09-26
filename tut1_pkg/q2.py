96c1fdccde0a5a1318864d68908873f3324c802c
Rohit <L00163278@student.lyit.ie>
2021-09-26 21:01:39 +0530

    added q2

---
+++ tut1_pkg/q2.py
@@ -0,0 +1,24 @@
+# -------------------------------------------
+# Testing variables
+# (C) 2021 Rohit Mishra
+# ---------------------------------------------
+
+
+top_icon = "^"
+bottom_icon = "v"
+left_icon = "<"
+right_icon = ">"
+
+print(top_icon * 30)
+print(left_icon + " " * 28 + right_icon)
+print(left_icon + " " * 28 + right_icon)
+print(left_icon + " " * 28 + right_icon)
+print(left_icon + " " * 11 + "Title"+" " * 12 + right_icon)
+print(left_icon + " " * 5 + "A Option a" + " " * 13 + right_icon)
+print(left_icon + " " * 5 + "B Option b" + " " * 13 + right_icon)
+print(left_icon + " " * 5 + "C Option c" + " " * 13 + right_icon)
+print(left_icon + " " * 28 + right_icon)
+print(left_icon + " " * 28 + right_icon)
+print(bottom_icon * 30)
+
+
